044f8e4d83ec6e92f6de230c07c91bd9e1698814
yojimbo <theyojimbo@gmail.com>
2009-08-07 10:26:59 +0900

    first commit

---
+++ app/models/product.rb
@@ -0,0 +1,20 @@
+class Product < ActiveRecord::Base
+  validates_presence_of :title, :description, :image_url
+  validates_numericality_of :price
+  validate :price_must_be_at_least_a_cent
+  validates_uniqueness_of :title, :message => 'This title already exists. '
+  validates_format_of :image_url, 
+                    :with => %r{\.(gif|jpg|png)$}i,
+					:message => 'must be a URL for GIF, JPG or PNG image.'
+  validates_length_of :title, :minimum => 10
+  
+    
+  def self.find_products_for_sale
+    find(:all, :order => "title")
+  end
+
+protected
+  def price_must_be_at_least_a_cent
+    errors.add(:price, 'should be at least 0.01') if price.nil? || price < 0.01
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,17 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_depot_session',
+  :secret      => '65702c0902bceacfa87229dfab3e00dd2008883bae0fa247bf79fecefaa570e653f464313dcb3b432f75df43328b4e52b8be2f6655ea2b18099583a544dcd8f8'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+ActionController::Base.session_store = :active_record_store
+
+
